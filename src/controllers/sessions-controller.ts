e85af595238ec13ad67c37bc58cc01e9cc30872b
frederico nakajima <fredericonakajimanakajima@gmail.com>
2026-04-03 13:36:11 -0300

    implementação do upload de fotos para perfil dos  customers

--- src/controllers/sessions-controller.ts
+++ src/controllers/sessions-controller.ts
@@ -1,49 +1,54 @@
-import { Request,Response } from "express"
-import { AppError } from "../utils/app-error"
-import { sign } from "jsonwebtoken"
-import { authConfig } from "../configs/auth"
-import { compare } from "bcrypt"
-import { z } from "zod"
-import { prisma } from "@/database/prisma"
+import { Request, Response } from "express";
+import { AppError } from "../utils/app-error";
+import { sign } from "jsonwebtoken";
+import { authConfig } from "../configs/auth";
+import { compare } from "bcrypt";
+import { z } from "zod";
+import { prisma } from "@/database/prisma";
 
-
-
-class SessionsController{
-  async create( request: Request, response: Response ){
-
-    const bodyschema = z.object({
-      email: z.string().email({message: "E-mail inválido"}),
+class SessionsController {
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email({ message: "E-mail inválido" }),
       password: z.string(),
-    })
+    });
 
-    const { email, password} = bodyschema.parse(request.body)   
+    const { email, password } = bodySchema.parse(request.body);
 
-    const user = await prisma.user.findFirst({where:{email}})
+    const user = await prisma.user.findFirst({ where: { email } });
 
-    if(!user){
-      throw new AppError("E-mail ou senha inválido",401)
+    if (!user) {
+      throw new AppError("E-mail ou senha inválido", 401);
     }
 
-    const passwordMatched = await compare(password,user.password)
+    const passwordMatched = await compare(password, user.password);
 
-    if(!passwordMatched){
-      throw new AppError("E-mail ou senha inválido",401)
+    if (!passwordMatched) {
+      throw new AppError("E-mail ou senha inválido", 401);
     }
 
-    const { secret, expiresIn } = authConfig.jwt
+    const { secret, expiresIn } = authConfig.jwt;
 
-    const token = sign({role:user.role},
+    const token = sign(
+      { role: user.role },
       secret,
       {
         subject: user.id,
-        expiresIn
+        expiresIn,
       }
-    )
+    );
 
-    const { password: _, ...userWithoutPassword } = user
-   
-    response.json({token, userWithoutPassword})
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      role: user.role,
+      availability: user.availability,
+      avatar: user.avatar,
+    };
+
+    response.json({ token, userWithoutPassword });
   }
 }
 
-export { SessionsController }
+export { SessionsController };
